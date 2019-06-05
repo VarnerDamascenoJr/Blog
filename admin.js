441417c563c3a556aec1080d2140466e55fe2c2c
VanerDamasceno <44266193+VanerDamasceno@users.noreply.github.com>
2019-06-05 14:46:33 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const mongoose = require('mongoose');
+require("../models/Categoria")
+const Categoria = mongoose.model("categoria")
+
+
 
 router.get('/',(req, res)=>{
   res.render("admin/index");
@@ -14,5 +19,17 @@ router.get('/categorias',(req, res)=>{
 router.get('/categorias/add', (req, res)=>{
   res.render('addcategoria')
 }
+router.post('/categoria/nova', (req, res)=>{
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+      console.log("Salvo com sucesso!");
+    }).catch((err)=>{
+      console.log("Erro ao salvar: "+err);
+    })
+})           
 
 module.exports = router;
