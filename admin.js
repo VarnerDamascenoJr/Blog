0d41b6d4855a1991d3b74f0fc24bdcec238d5c86
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-06-06 10:55:03 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -20,16 +20,33 @@ router.get('/categorias/add', (req, res)=>{
   res.render('addcategoria')
 }
 router.post('/categoria/nova', (req, res)=>{
-    const novaCategoria = {
-      nome: req.body.nome,
-      slug: req.body.slug
+  
+    var erros = []
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+      erros.push({nome:"nome invalido"})
     }
-
-    new Categoria(novaCategoria).save().then(()=>{
-      console.log("Salvo com sucesso!");
-    }).catch((err)=>{
-      console.log("Erro ao salvar: "+err);
-    })
-})           
+    if(!req.body.slug || req.body.slug == undefined || req.body.slug == null){
+      erros.push({texto:"Slug invalido"})
+    }
+    if(req.body.nome.length < 2){
+      erros.push({texto:"Nome da categoria muito pequeno"})
+    }
+    if(erros.length >0){
+      res.render("admin/addcategorias", {erros:erros})
+    }else{
+      const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+      }
+  
+      new Categoria(novaCategoria).save().then(()=>{
+        req.flash("success_msg","categoria adicionada com sucesso")
+        res.redirect("/admin/categorias")
+      }).catch((err)=>{
+        req.flash("error_msg", "tente novamente")
+        console.log("Erro ao salvar: "+err);
+      })
+    }
+})
 
 module.exports = router;
