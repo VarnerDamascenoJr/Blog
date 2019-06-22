7ed9bf18079b7d6bfedf93696320985952f75504
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-06-21 19:29:06 -0500

    Adição do model para postagem.
    Aqui foi adicionado o model para postagem com toda a sua organização.

--- models/Postagem.js
+++ models/Postagem.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose')
-const Schema   = mongoose.Schema
-const Postagem = new Schhema({
-      tituto:{type: String, required: true},
-      slug:{type: String, required: true},
-      descricao:{type:String, required: true},
-      conteudo:{type: String, required:true},
-      categoria:{type: Schema.ObjetcId, ref: "categorias", required: true}
-      data:{type:Date,default:Date.now()}
+const Schema   = mongoose.Schema;
+
+
+const Postagem = new Schema({
+   titulo:{
+     type:String,
+     required:true
+   },
+   slug:{
+     type: String,
+     required: true
+   },
+   descricao:{
+     type:String,
+     required: true
+   },
+   conteudo:{
+     type: String,
+     required: true
+   },
+   categoria:{
+     //fará referência a uma postagem
+     //Ou seja, armazerá o ID de um objeto
+     type: Schema.Types.ObjectId,
+     ref:"categorias",
+     required: true
+   },
+   data:{
+     type: Date,
+     default:Date.now()
+   }
+
 })
 
 mongoose.model("postagens", Postagem)
