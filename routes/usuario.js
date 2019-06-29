0d1e59747d061cf5ab6698f9a08d96c416139fcb
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-06-29 17:36:15 -0500

    Sobre o cadastramento do admin

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,7 +3,7 @@ const router  = express.Router()
 const mongoose= require('mongoose')
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
-
+const bcrypt = require('bcryptjs')
 
 router.get("/registro", (req, res)=>{
   res.render("usuarios/registro")
@@ -15,12 +15,56 @@ router.post("/registro",(req, res)=>{
   if(!req.boby.nome || typeof req.body.nome == undefined || req.boby.nome == null){
     erros.push({texto:"Nome inválido"})
   }
-  f(!req.boby.email || typeof req.body.email == undefined || req.boby.email == null){
+  if(!req.boby.email || typeof req.body.email == undefined || req.boby.email == null){
     erros.push({texto:"E-mail inválido"})
   }
-  f(!req.boby.senha || typeof req.body.senha == undefined || req.boby.senha == null){
+  if(!req.boby.senha || typeof req.body.senha == undefined || req.boby.senha == null){
     erros.push({texto:"Senha inválido"})
   }
+
+  if(!req.boby.senha.length < 4){
+    erros.push({texto:"Senha muito curta"})
+  }
+  if(!req.boby.senha != req.body.senha2){
+    erros.push({texto:"Senhas diferentes, tente novamente."})
+  }
+  if(erros.length > 0){
+    res.render("usuarios/registro",{erros:erros})
+
+  }else {
+    //verificar se o usuário que está tentando se cadastrar, se o email dele já existe
+    //pesquisar por emal
+    Usuario.findOne({email: req.body.email}).then((usuario)=>{
+      if (usuario) {
+        req.flash("error_msg","Já existe uma conta com este email no sistema.")
+        res.redirect("usuarios/registro")
+      }else {
+        cont novousuario = new Usuario({
+          nome: req.body.nome,
+          email: req.body.email,
+          senha: req.body.senha
+        })
+
+        //nao posso salvar o objeto antes de encriptar a senha.
+        //Assim, irei encriptar para dar maior segurança no trato com a senha
+        bcrypt.genSalt(10, (erro, salt)=>{
+          bcrypt.hash(novousuario.senha, salt, (erro, hash)=>{
+            if(erro){req.flash("error_msg","Houve um erro ao salvar") res.redirect("/")}
+            //Aqui pegará a nova senha
+            novousuario.senha = hash
+            novousuario.save().then(()=>{
+              req.flash("success_msg", "Salvo com sucesso") res.redirect("/")
+            }).catch((err)=>{
+              req.flash("error_msg"," Houve um erro ao criar") res.redirect("/usuarios/registro")
+            })
+
+          })//funcao com três parâmetros
+
+        })
+      }
+    }).catch((err)=>{ req.flash("error_msg","Houve um erro interno") res.redirect("/")})
+
+  }//fim else
 })
 
 model.exports = router
