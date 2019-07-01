d7fce81f90b39a50cce622293a56e0d54bc5cdb7
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-06-30 19:55:09 -0500

    Configuração do passaporte

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const Postagem = mongoose.model("postagens")
 require("./models/Categoria")
 const Categoria = mongoose.model("categorias")
 const usuarios = require('./routes/usuario')
+const passport = require('passport')
+require('/config/auth')(passport)
 //Configurações
    //Sessão
      app.use(session({
@@ -21,6 +23,10 @@ const usuarios = require('./routes/usuario')
        resave: true,
        saveUninitialized: true
      }))
+     //deve ficar nesta ordem
+     app.use(passport.initialize())
+     app.use(passport.session())
+
      app.use(flash())
    //Middleware
      app.use((req, res, next)=>{
