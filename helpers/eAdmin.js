fc3885d265e86bb64c803c0655bc074ba711c6f7
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-09-25 18:13:07 -0500

    Update eAdmin.js

--- helpers/eAdmin.js
+++ helpers/eAdmin.js
@@ -1,7 +1,7 @@
 //A funcão desta parte é verificar se está autenticado e se é administrador
 module.exports = {
   eAdmin: function(req, res, next){
-    if(req.isAuthenticated() && req.user.eAdmin ==1){
+    if(req.isAuthenticated() && req.user.eAdmin == 1){
       return next();
     }
     req.flash("error_msg", "Você não fez o login corretamente")
